6da56ffe7c0f44c2e903b6fd7a9d04e57a68a8fe
MaZderMind <github@mazdermind.de>
2014-07-06 14:19:32 +0200

    use a serializer which returns the id

--- polls/rest.py
+++ polls/rest.py
@@ -1,11 +1,17 @@
-from rest_framework import viewsets, routers
+from rest_framework import viewsets, routers, serializers
 from polls import models
 
-# ViewSets define the rest-api behavior
+# the serializer specifies how the records are serialized in the list or detail-views
+class PollSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = models.Poll
+		fields = ('id', 'url', 'question', 'pub_date', )
+
+# the viewset defines urls and views for list and detail-viwes
 class PollViewSet(viewsets.ModelViewSet):
 	model = models.Poll
+	serializer_class = PollSerializer
 
 # register viewsets for the rest-api
 def register(restrouter):
 	restrouter.register(r'polls', PollViewSet)
-
