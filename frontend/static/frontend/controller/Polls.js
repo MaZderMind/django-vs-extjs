6fe46407e1720c74759f59a7d592646ef99bcdcf
MaZderMind <github@mazdermind.de>
2014-07-06 22:18:03 +0200

    add polls controller & view

---
+++ frontend/static/frontend/controller/Polls.js
@@ -0,0 +1,11 @@
+Ext.define('MyApp.controller.Polls', {
+	extend: 'Ext.app.Controller',
+	views: ['Polls'],
+
+	init: function() {
+		this.view = this.getView('Polls').create();
+
+		MyApp.getApplication().getController('Navigation')
+			.registerNavigationItem('polls', 'poll-management', this.view);
+	}
+});
---
+++ frontend/static/frontend/view/Polls.js
@@ -0,0 +1,6 @@
+Ext.define('MyApp.view.Polls', {
+	extend: 'Ext.panel.Panel',
+	id: 'polls',
+	title: 'Polls',
+	html: 'some poll content'
+});
