95d3a7cd6c3289a076355c72667720ad2e69196e
MaZderMind <github@mazdermind.de>
2014-07-09 20:14:05 +0200

    use queryset instead of model specification

--- polls/rest.py
+++ polls/rest.py
@@ -1,15 +1,15 @@
 from rest_framework import viewsets, routers, serializers
-from polls import models
+from polls.models import Poll
 
 # the serializer specifies how the records are serialized in the list or detail-views
 class PollSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
-		model = models.Poll
+		model = Poll
 		fields = ('id', 'url', 'question', 'pub_date', )
 
 # the viewset defines urls and views for list and detail-viwes
 class PollViewSet(viewsets.ModelViewSet):
-	model = models.Poll
+	queryset = Poll.objects.all()
 	serializer_class = PollSerializer
 
 # register viewsets for the rest-api
